30af14da0514c5369e4c80bde06f26a0bd46ebe7
Alex De Meo <alexander.demeo@ocsbstudent.ca>
2019-11-06 15:56:44 -0500

    fished math quiz

---
+++ MathQuizAlex/main.lua
@@ -0,0 +1,33 @@
+-- Title: Math Quiz
+-- Name: Alex De Meo
+-- Course: ICS2O
+-- This program displays a math problem and allow the user to enter the answer
+
+-- hide Status Bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- Background Color
+display.setDefault ('Background', )
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+--------------------------------------------------------------------------------------------------------------------------------------------------
+local dingSound
+local buzzSound
+local youWinMusic
+local youWinMusicChannel
+local gameOverMusic
+local gameOverMusicChannel
+local checkmark
+local redX
+local 
+local Play
+local questionObject
+local userAnswer
+local correctAnswer
+local correctObject
+local incorrectObject
+local numericField
+local randomNum1
+local randomNum2
+local randomOperator
+local divMultiAns
